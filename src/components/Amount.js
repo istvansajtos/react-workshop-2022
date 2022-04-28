f59efadb8b7d12fec4333339b3299e3a73b1e4b5
Norbert Nemeth <norbert.nemeth@liferay.com>
2022-04-28 10:15:45 +0200

    feat(step 03): Show red outline if the value is negative

--- src/components/Amount.js
+++ src/components/Amount.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './amount.css';
 
 export default function Amount({name}) {
-  return (
-      <label>
-        <span>{name}</span>
-        <input placeholder="0" step="0.1" type="number" />
-      </label>
-  );
+    const [isNegative, setIsNegative] = useState(false);
+
+    const onChangeHandler = (event) => {
+        setIsNegative(event.currentTarget.value < 0);
+    }
+
+    return (
+        <label className="amount">
+            <span>{name}</span>
+            <input
+                className={isNegative ? 'negative' : ''}
+                onChange={onChangeHandler}
+                placeholder="0"
+                step="0.1"
+                type="number"
+            />
+        </label>
+    );
 }
