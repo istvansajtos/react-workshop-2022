c38eeb1b060c4ee82a7c6656f4b97dd5ef854582
Norbert Nemeth <norbert.nemeth@liferay.com>
2022-04-28 11:10:08 +0200

    feat(step 06): Use setTimeout to crash the BTC price

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Amount from './components/Amount';
 
@@ -10,9 +10,25 @@ function App() {
 	const [value, setValue] = useState(0);
 	const [exchangeRate, setExchangeRate] = useState(calcExchangeRate);
 
+	useEffect(() => {
+		let timer = setTimeout(()=>{setExchangeRate(0)}, 5000);
+
+		return () => {
+			clearTimeout(timer);
+		}
+	},[value]);
+
+	const onChange = (value) => {
+		setValue(value);
+
+		if (exchangeRate === 0) {
+			setExchangeRate(calcExchangeRate);
+		}
+	}
+
 	return (
     	<div className="App">
-			<Amount name="Euros" value={value} onChange={setValue}/>
+			<Amount name="Euros" value={value} onChange={onChange}/>
 			<Amount name="BTC" value={parseFloat(value * exchangeRate).toFixed(4)} disabled />
     	</div>
 	);
