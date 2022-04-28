8e7b14e32d0898d181afe70d986a5b7989cb3b2c
Norbert Nemeth <norbert.nemeth@liferay.com>
2022-04-28 09:57:18 +0200

    feat(step 01): Add Euros input field

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+		<label>
+			<span>Euros</span>
+			<input placeholder="0" step="0.1" type="number" />
+		</label>
     </div>
   );
 }
