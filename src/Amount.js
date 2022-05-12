6f88cf6609a9d6aaf2717bc327e01fedff16d6dc
Istvan Sajtos <istvan.sajtos@liferay.com>
2022-05-13 00:59:30 +0200

    step 3: Teach <Amount /> input to show a red outline for negative amounts.

--- src/Amount.js
+++ src/Amount.js
@@ -1,8 +1,14 @@
+import React, { useState } from 'react';
+import './Amount.css';
+
 function Amount() {
+  const [negative, setNegative] = useState(false);
+  const handleChange = event => setNegative(event.target.value < 0 ? true : false);
+
   return (
     <div>
-      <label for="euros">Euros</label>
-      <input id="euros" type="number" />
+      <label htmlFor="euros">Euros</label>
+      <input id="euros" type="number" className={negative ? 'redborder' : ""} onChange={handleChange} />
     </div>
   );
 }
