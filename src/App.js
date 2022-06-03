fd4b401e5e4a97903dacc631bfd0c389cda31c99
Istvan Sajtos <istvan.sajtos@liferay.com>
2022-06-04 00:02:45 +0200

    fixup

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import ThemeContext, { themes }  from './ThemeContext.js';
 
 function App() {
   const [theme, setTheme] = useState(themes.light);
+  const [premium, setPremium] = useState(false);
 
   useEffect(()=> {
     fetch('http://localhost:3003/data')
@@ -14,6 +15,11 @@ function App() {
     })
   }, [])
 
+  const premiumSection =
+    premium === true ?
+      <span><g-emoji class="g-emoji" alias="gem" fallback-src="https://github.githubassets.com/images/icons/emoji/unicode/1f48e.png">💎</g-emoji> Premium conversion</span> : 
+      <button type="button" onClick={() => setPremium(true)}>Become Premium</button>;
+
   return (
     <div className="App">
       <ThemeContext.Provider value={theme}>
@@ -21,12 +27,16 @@ function App() {
         <Converter cryptoName="$ETH" exchangeRate="1.2" header={<strong>ETH converter</strong>} />
       </ThemeContext.Provider>
 
-      <br/>
+      <br/><br/>
 
       <select onChange={event => setTheme(event.target.value == "light" ? themes.light : themes.dark)} >
           <option value="light">Light</option>
 					<option value="dark">Dark</option>
 			</select>
+
+      <br/><br/>
+
+      {premiumSection}
     </div>
   );
 }
